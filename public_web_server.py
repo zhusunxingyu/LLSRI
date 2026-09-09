0555b72293c6242de05371d146ec18219f283e77
zhusunxingyu <17887381308@163.com>
2026-09-09 19:41:22 +0800

    Add files via upload

--- public_web_server.py
+++ public_web_server.py
@@ -34,6 +34,9 @@ class PublicWebHandler(BaseHTTPRequestHandler):
         self.send_header("Access-Control-Allow-Origin", "*")
         self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
         self.end_headers()
         self.wfile.write(body)
 
